269e2400290087a12ed49f25292ce2e43686f0c7
muspecialist <55097482+muspecialist@users.noreply.github.com>
2024-01-18 12:56:10 -0500

    my information v1

---
+++ myInfo.py
@@ -0,0 +1,7 @@
+#CS175L
+#Your first name and Last name
+#myInfo
+
+print('My name is: Gil Eckert')
+print('My address is: 1234 Central Ave, Bismark, North Dakota, 12345')
+print('My phone number is: 123-456-9877')
